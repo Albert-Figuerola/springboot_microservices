30d42a100a870fa1318ce54ae1d6b8ae1d2c9993
Albert <afiguerola@gaddexapp.com>
2025-11-13 18:46:01 +0100

    First commit.

---
+++ msvc-products/src/main/java/com/springcloud/msvc/products/controllers/ProductController.java
@@ -0,0 +1,35 @@
+package com.springcloud.msvc.products.controllers;
+
+import com.springcloud.msvc.products.entities.Product;
+import com.springcloud.msvc.products.services.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1")
+@CrossOrigin(origins = "*")
+public class ProductController {
+
+    final private ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping("products")
+    public ResponseEntity<?> list() {
+        return ResponseEntity.ok(this.service.findAll());
+    }
+
+    @GetMapping("products/{id}")
+    public ResponseEntity<?> detail(
+            @PathVariable Long id
+    ) {
+        Optional<Product> product = this.service.findById(id);
+
+        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+}
