05ae7945ca7f67a00dd038a37a57dc9dc43bce49
Albert <afiguerola@gaddexapp.com>
2025-11-17 20:19:44 +0100

    Implement service with feign client

--- msvc-items/src/main/java/org/springcloud/msvc/items/clients/ProductFeignClient.java
+++ msvc-items/src/main/java/org/springcloud/msvc/items/clients/ProductFeignClient.java
@@ -2,19 +2,18 @@ package org.springcloud.msvc.items.clients;
 
 import org.springcloud.msvc.items.models.Product;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "localhost:8001")
+@FeignClient(url = "localhost:8001", name = "msvc-products")
 public interface ProductFeignClient {
 
-    @GetMapping("products")
+    @GetMapping("api/v1/products")
     List<Product> findAll();
 
-    @GetMapping("products/{id}")
+    @GetMapping("api/v1/products/{id}")
     Product findById(@PathVariable Long id);
 
 }
